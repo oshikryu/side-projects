2937b56e6c3fea10862a8f1f408185b0c3e5ea9f
Ryuta Oshikiri <ryuta.oshikiri@truepill.com>
2020-12-22 21:49:50 -0800

    fixed

--- assorted-js/addTwoNumbers.js
+++ assorted-js/addTwoNumbers.js
@@ -1,3 +1,20 @@
+var addTwoNumbers = function(l1, l2) {
+    let node = null
+    const carry = arguments[2]
+    if (l1 || l2) {
+        const val1 = l1 ? l1.val : 0
+        const val2 = l2 ? l2.val : 0
+        const next1 = l1 ? l1.next : null
+        const next2 = l2 ? l2.next : null
+        const val = carry ? val1 + val2 + 1 : val1 + val2
+        node = new ListNode(val % 10)
+        node.next = addTwoNumbers(next1, next2, val >= 10)  
+    } else if (carry) {
+        node = new ListNode(1)
+        node.next = null
+    }
+    return node
+};
 
 //
 // not working solution
